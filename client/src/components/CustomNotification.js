15be45690c9e8486d119b593611d804fadbcbc7a
Aya Bochman <aya@Ayas-MacBook-Pro.local>
2021-10-10 19:41:58 +0200

    added custom notification box and logic for text

--- client/src/components/CustomNotification.js
+++ client/src/components/CustomNotification.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
-const CustomNotification = (props) => {
-    const {type, message, iconSrc, color} = props;
-  return <StyledNotification color={color}></StyledNotification>;
+const CustomNotification = ({ props }) => {
+  const { type, message, color } = props;
+  const [boxColor, setBoxColor] = useState('');
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    if (type && message && color) {
+      setBoxColor(color);
+      setText(handleText(type, message));
+    }
+  }, [type, message, color]);
+
+  const handleText = (type, text) => {
+    return `${type && type.charAt(0).toUpperCase() + type.slice(1)}: ${text}`;
+  };
+
+  return (
+    <>
+      {props ? (
+        <StyledNotification color={boxColor}>{text}</StyledNotification>
+      ) : null}
+    </>
+  );
 };
 
 export default CustomNotification;
 
 const StyledNotification = styled.div`
-width: 500px;
-height: 100px;
-background-color: ${props => props.color || 'blue'};
-`
+  width: 500px;
+  height: 100px;
+  margin: 0 auto;
+  border-radius: 20px;
+  background-color: ${(props) => props.color || ''};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -5,20 +5,68 @@ import CustomNotification from './CustomNotification';
 
 const Main = () => {
   const notifications = useSelector((state) => state.notifications.data);
+  const config = useSelector((state) => state.configurations.data);
 
   const [notificationsData, setNotificationsData] = useState([]);
+  const [randNotification, setRandNotification] = useState({});
 
   useEffect(() => {
-    if (notifications?.length) {
+    console.log('condig', config);
+    if (notifications?.length && config) {
       setNotificationsData(notifications);
-      console.log('notifications here', notifications);
+      getRandomNotification(notifications, config);
     }
-  }, [notifications]);
+  }, [notifications, config]);
+
+  const getRandomNotification = (arr, config) => {
+    const { minDuration, maxDuration } = config;
+
+    const randomNotification = getRandomArrItem(arr);
+    const notificationDuration = getNumberFromRange(minDuration, maxDuration);
+
+    const notif = {
+      type: randomNotification.type,
+      color: randomNotification.color,
+      message: handleMessage(randomNotification.message),
+      duration: notificationDuration,
+    };
+    console.log('notification final====', notif);
+    setRandNotification(notif);
+  };
+
+  const getRandomArrItem = (arr) => {
+    if (arr.length > 1) {
+      return arr[Math.floor(Math.random() * arr.length)];
+    }
+    return arr[0];
+  };
+
+  const getNumberFromRange = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+
+  const handleMessage = (message) => {
+    const randMessage = getRandomArrItem(message);
+    let str = '';
+    if (randMessage.toLowerCase().includes('sale')) {
+      str = `${randMessage}!`;
+    } else if (randMessage.toLowerCase().includes('new')) {
+      str = `~~${randMessage}`;
+    } else if (randMessage.toLowerCase().includes('limited edition')) {
+      const splitted = randMessage.toLowerCase().split('limited edition');
+      str = `${splitted[0]}LIMITED EDITION${splitted[1]}`;
+    } else {
+      str = randMessage;
+    }
+    return str;
+  };
 
   return (
     <StyledMain>
       <h1>Custom Notifications</h1>
-      <CustomNotification />
+      {randNotification ? (
+        <CustomNotification props={randNotification} />
+      ) : null}
     </StyledMain>
   );
 };
